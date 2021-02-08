9c9be05f4a25767d321a77640cccf6e4500817b6
Lokesh Singhania <singhania7lokesh@gmail.com>
2021-02-08 19:33:11 +0530

    Initial Commit

---
+++ src/main/java/algo/linked/list/SplitInParts.java
@@ -0,0 +1,37 @@
+package algo.linked.list;
+
+public class SplitInParts {
+
+	ListNode[] splitListIntoParts(ListNode head, int k) {
+		int n = size(head);
+		ListNode node = head;
+		ListNode[] parts = new ListNode[k];
+		int p = 0;
+		int remaining = n;
+		while (remaining > 0) {
+			parts[p] = node;
+			int partSize = divideEqually(remaining, k - p);
+			for (int j = 1; j < partSize; j++, node = node.next, remaining--);
+			ListNode buffer = node.next;
+			node.next = null;
+			node = buffer;
+			remaining--;
+			p++;
+		}
+		return parts;
+	}
+
+	int size(ListNode root) {
+		int i = 0;
+		ListNode node = root;
+		while (node != null) {
+			node = node.next;
+			i++;
+		}
+		return i;
+	}
+
+	int divideEqually(int totalItems, int parts) {
+		return totalItems % parts == 0 ? totalItems / parts : totalItems / parts + 1;
+	}
+}
