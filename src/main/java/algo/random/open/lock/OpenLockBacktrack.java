5d5b2d83eaad6390426e0b0cda2ec7e763a8254c
Lokesh Singhania <lkssgn@gmail.com>
2024-07-18 13:34:56 +0530

    Raise Java Version: 21

---
+++ src/main/java/algo/random/open/lock/OpenLockBacktrack.java
@@ -0,0 +1,10 @@
+package algo.random.open.lock;
+
+public class OpenLockBacktrack implements OpenLock{
+    @Override
+    public int openLock(String[] deadends, String target) {
+        return 0;
+    }
+
+   // class Backtrack
+}
